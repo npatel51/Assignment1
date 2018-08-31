654b8ac920fd63d78254b6b46e6475b615cfa618
Niraj <npatel3@ufl.edu>
2018-08-31 17:09:04 -0400

    initial commit

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ var listingData, server;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
-
+  if( request.method === 'GET' && parsedUrl.pathname === '/listing'){
+    console.log("/listing");
+  } else {
+    
+  }
   /*
     Your request handler should send listingData in the JSON format if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -17,6 +21,11 @@ var requestHandler = function(request, response) {
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
 };
+
+http.createServer(function(request,response){
+  console.log("Server created");
+  requestHandler(request,response);
+}).listen(8080);
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
